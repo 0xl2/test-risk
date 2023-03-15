c6581ac2065118cb82de7ef790c9496aa956d175
naruto0913 <abe.sinjiro7@gmail.com>
2023-03-16 00:48:56 +0900

    feat: websocket connected

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,43 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useEffect, useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+// init socket url
+const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL || ""
 
 export default function Home() {
+  const [obList, setOblist] = useState([])
+
+  useEffect(() => {
+    // init ws
+    const ws = new WebSocket(socketUrl)
+    
+    // function for ws open
+    ws.onopen = () => {
+      console.log(`websocekt connection opened`)
+      ws.send(`hello server`)
+    }
+
+    // update when receive msg from socket server
+    ws.onmessage = (event: any) => {
+      console.log(`received message: ${event.data}`)
+
+      // handle event data
+    }
+
+    // send subscribe request
+    ws.send(JSON.stringify({
+      "type": "subscribe",
+      "channel": "orders",
+      "requestId": "test-request-id"
+    }))
+
+    // close socket when page close
+    return () => {
+      ws.close()
+    }
+  }, [])
+
   return (
     <>
       <Head>
