31a3673294ca657e9e038ec6199c9b3b8ffeba85
naruto0913 <abe.sinjiro7@gmail.com>
2023-03-16 01:33:51 +0900

    feat: add tokens

---
+++ constants/tokens.ts
@@ -0,0 +1,19 @@
+interface ITokens {
+    symbol: string,
+    address: string
+}
+
+export const tokens: ITokens[] = [
+    {
+        symbol: 'USDC',
+        address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+    },
+    {
+        symbol: 'USDT',
+        address: '0xdac17f958d2ee523a2206206994597c13d831ec7'
+    },
+    {
+        symbol: 'WBTC',
+        address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'
+    }
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,32 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import { useEffect, useState } from 'react'
 
+import { tokens } from '../../constants/tokens'
+
 // init socket url
 const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL || ""
 
+interface IPayload {
+  takerToken: string | null,
+  makerToken: string | null
+}
+
+interface IReqBody {
+  type: string,
+  channel: string,
+  requestId: string,
+  payload: IPayload | null
+}
+
 export default function Home() {
+  const [makerToken, setMakerToken] = useState("")
+  const [takerToken, setTakerToken] = useState("")
   const [obList, setOblist] = useState([])
+  const [ws, setWs] = useState(null as any)
 
   useEffect(() => {
     // init ws
-    const ws = new WebSocket(socketUrl)
+    setWs(new WebSocket(socketUrl))
     
     // function for ws open
     ws.onopen = () => {
@@ -25,18 +42,37 @@ export default function Home() {
       // handle event data
     }
 
-    // send subscribe request
-    ws.send(JSON.stringify({
-      "type": "subscribe",
-      "channel": "orders",
-      "requestId": "test-request-id"
-    }))
-
     // close socket when page close
     return () => {
       ws.close()
     }
   }, [])
+
+  const optChanged = () => {
+    let payload = {
+      type: "subscribe",
+      channel: "orders",
+      requestId: "test-request-id"
+    } as IReqBody
+
+    // add takerToken in payload if selected
+    if (takerToken.length > 0) {
+      payload.payload = {
+        takerToken
+      } as IPayload
+    }
+
+    // add makerToken in payload if selected
+    if (makerToken.length > 0) {
+      if (payload.payload) payload.payload.makerToken = makerToken
+      else payload.payload = {
+        makerToken
+      } as IPayload
+    } 
+      
+    // send subscribe request
+    ws.send(JSON.stringify(payload))
+  }
 
   return (
     <>
