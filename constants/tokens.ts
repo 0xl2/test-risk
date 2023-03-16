06bd7d4d45da7f66b01b6d154378796d643d3215
naruto0913 <abe.sinjiro7@gmail.com>
2023-03-16 10:14:35 +0900

    feat: right sides are integrated

--- constants/tokens.ts
+++ constants/tokens.ts
@@ -1,19 +1,88 @@
-interface ITokens {
-    symbol: string,
-    address: string
-}
+import { IToken } from './interface'
 
-export const tokens: ITokens[] = [
+export const tokens: IToken[] = [
     {
         symbol: 'USDC',
+        name: 'USD Coin',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png',
         address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
     },
     {
+        name: 'Tether USD',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
         symbol: 'USDT',
         address: '0xdac17f958d2ee523a2206206994597c13d831ec7'
     },
     {
+        name: 'Wrapped Bitcoin',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3717.png',
         symbol: 'WBTC',
         address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'
+    },
+    {
+        name: 'Curve',
+        symbol: 'CRV',
+        address: '0xD533a949740bb3306d119CC777fa900bA034cd52',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/6538.png'
+    },
+    {
+        name: 'Uniswap',
+        symbol: 'UNI',
+        address: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png'
+    },
+    {
+        name: 'Chainlink',
+        symbol: 'LINK',
+        address: '0x514910771af9ca656af840dff83e8264ecf986ca',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1975.png'
+    },
+    {
+        name: '0x',
+        symbol: 'ZRX',
+        address: '0xe41d2489571d322189246dafa5ebde1f4699f498',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1896.png'
+    },
+    {
+        name: 'renBTC',
+        symbol: 'RENBTC',
+        address: '0xeb4c2781e4eba804ce9a9803c67d0893436bb27d',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5777.png'
+    },
+    {
+        name: 'Compound',
+        symbol: 'COMP',
+        address: '0xc00e94cb662c3520282e6f5717214004a7f26888',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5692.png'
+    },
+    {
+        name: 'Balancer',
+        symbol: 'BAL',
+        address: '0xba100000625a3754423978a60c9317c58a424e3D',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5728.png'
+    },
+    {
+        name: 'Aave',
+        symbol: 'AAVE',
+        address: '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/7278.png'
+    },
+    {
+        name: 'Augur',
+        symbol: 'REPV2',
+        address: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1104.png'
+    },
+    {
+        name: 'Ren',
+        symbol: 'REN',
+        address: '0x408e41876cccdc0f92210600ef50372656052a38',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/2539.png'
+    },
+    {
+        name: 'Curate',
+        symbol: 'XCUR',
+        address: '0xd52669712f253CD6b2Fe8A8638F66ed726cb770C',
+        logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/7942.png'
     }
 ]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,78 +1,115 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+import { Collapse } from "react-collapse";
+import { Search } from "@mui/icons-material";
+import { Container, Grid, TextField, InputAdornment } from "@mui/material";
 
-import { tokens } from '../../constants/tokens'
+import Head from "next/head";
+
+import { tokens } from "../../constants/tokens";
+import { IToken } from "../../constants/interface";
+
+import styles from "@/styles/Home.module.css";
 
 // init socket url
-const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL || ""
+const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL || "";
 
 interface IPayload {
-  takerToken: string | null,
-  makerToken: string | null
+  takerToken: string | null;
+  makerToken: string | null;
 }
 
 interface IReqBody {
-  type: string,
-  channel: string,
-  requestId: string,
-  payload: IPayload | null
+  type: string;
+  channel: string;
+  requestId: string;
+  payload: IPayload | null;
 }
 
 export default function Home() {
-  const [makerToken, setMakerToken] = useState("")
-  const [takerToken, setTakerToken] = useState("")
-  const [obList, setOblist] = useState([])
-  const [ws, setWs] = useState(null as any)
+  const [, setSearch] = useState("");
+  const [opt, setOpt] = useState("");
+  const [open, setOpen] = useState(true);
+  const [obList, setOblist] = useState([]);
+  const [makerToken, setMakerToken] = useState<IToken | undefined>(undefined);
+  const [takerToken, setTakerToken] = useState<IToken | undefined>(undefined);
+  const [filtered, setFiltered] = useState([] as IToken[]);
+
+  // useEffect(() => {
+  //   // init ws
+  //   const ws = new WebSocket(socketUrl);
+
+  //   // function for ws open
+  //   ws.onopen = () => {
+  //     console.log(`websocekt connection opened`);
+  //   };
+
+  //   // update when receive msg from socket server
+  //   ws.onmessage = (event: any) => {
+  //     console.log(`received message: ${event.data}`);
+
+  //     // handle event data
+  //   };
+
+  //   // close socket when page close
+  //   return () => {
+  //     ws.close();
+  //   };
+  // }, []);
 
   useEffect(() => {
-    // init ws
-    setWs(new WebSocket(socketUrl))
-    
-    // function for ws open
-    ws.onopen = () => {
-      console.log(`websocekt connection opened`)
-      ws.send(`hello server`)
+    setFiltered(tokens);
+  }, []);
+
+  const optChanged = (token: IToken) => {
+    console.log(token, opt);
+    if (opt == "maker") {
+      setMakerToken(token);
+    } else if (opt == "taker") {
+      setTakerToken(token);
     }
 
-    // update when receive msg from socket server
-    ws.onmessage = (event: any) => {
-      console.log(`received message: ${event.data}`)
-
-      // handle event data
-    }
-
-    // close socket when page close
-    return () => {
-      ws.close()
-    }
-  }, [])
-
-  const optChanged = () => {
     let payload = {
       type: "subscribe",
       channel: "orders",
-      requestId: "test-request-id"
-    } as IReqBody
+      requestId: "test-request-id",
+    } as IReqBody;
 
     // add takerToken in payload if selected
-    if (takerToken.length > 0) {
+    if (takerToken) {
       payload.payload = {
-        takerToken
-      } as IPayload
+        takerToken: takerToken.address,
+      } as IPayload;
     }
 
     // add makerToken in payload if selected
-    if (makerToken.length > 0) {
-      if (payload.payload) payload.payload.makerToken = makerToken
-      else payload.payload = {
-        makerToken
-      } as IPayload
-    } 
-      
+    if (makerToken) {
+      if (payload.payload) payload.payload.makerToken = makerToken.address;
+      else
+        payload.payload = {
+          makerToken: makerToken.address,
+        } as IPayload;
+    }
+
     // send subscribe request
-    ws.send(JSON.stringify(payload))
-  }
+    // ws.send(JSON.stringify(payload));
+
+    setOpen(true);
+  };
+
+  const doChange = (searchVal: string) => {
+    setSearch(searchVal);
+
+    const lowSearchVal = searchVal.toLowerCase();
+    const newFilter = tokens.filter((tokenItem: IToken) => {
+      return (
+        tokenItem.symbol.toLocaleLowerCase().includes(lowSearchVal) ||
+        tokenItem.name.toLocaleLowerCase().includes(lowSearchVal) ||
+        tokenItem.address.toLocaleLowerCase().includes(lowSearchVal)
+      );
+    });
+
+    setFiltered(newFilter);
+  };
 
   return (
     <>
@@ -82,11 +119,123 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.center}>
-          
-        </div>
-      </main>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={8}></Grid>
+          <Grid item xs={4}>
+            <div className={styles.selectDiv}>
+              <Collapse className="collapse" isOpened={open}>
+                <div className={styles.cardDiv}>
+                  <p className={styles.title}>Select tokens</p>
+                </div>
+                <div className={styles.cardDiv}>
+                  <button
+                    className={styles.tokenBtn}
+                    onClick={() => {
+                      setOpt("maker");
+                      setOpen(false);
+                    }}
+                  >
+                    {makerToken ? makerToken.name : "Maker Token"}
+                    <span className={styles.btnSpan}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="11"
+                        height="7"
+                        fill="none"
+                        viewBox="0 0 11 7"
+                      >
+                        <path
+                          fill="#1F1F41"
+                          d="M5.4 6.9L0 1.5 1.4.1l4 4 4-4 1.4 1.4-5.4 5.4z"
+                        ></path>
+                      </svg>
+                    </span>
+                  </button>
+                </div>
+                <div className={styles.cardDiv}>
+                  <button
+                    className={styles.tokenBtn}
+                    onClick={() => {
+                      setOpt("taker");
+                      setOpen(false);
+                    }}
+                  >
+                    {takerToken ? takerToken.name : "Taker Token"}
+                    <span className={styles.btnSpan}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="11"
+                        height="7"
+                        fill="none"
+                        viewBox="0 0 11 7"
+                      >
+                        <path
+                          fill="#1F1F41"
+                          d="M5.4 6.9L0 1.5 1.4.1l4 4 4-4 1.4 1.4-5.4 5.4z"
+                        ></path>
+                      </svg>
+                    </span>
+                  </button>
+                </div>
+              </Collapse>
+              <Collapse className="collapse" isOpened={!open}>
+                <div className={styles.collapseHead}>
+                  <div className={styles.cardDiv}>
+                    <p className={styles.tokenTitle}>Choose token</p>
+                  </div>
+                  <div className={styles.jmgGTH} onClick={() => setOpen(true)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="12"
+                      height="12"
+                      fill="none"
+                      viewBox="0 0 10 10"
+                    >
+                      <path
+                        fill="#1F1F41"
+                        d="M8.18 9.278a.75.75 0 001.065-1.056L5.81 4.755 9.278 1.32A.75.75 0 108.222.253L4.754 3.69 1.317.222A.75.75 0 10.252 1.278l3.437 3.468L.222 8.18a.75.75 0 001.056 1.066L4.744 5.81 8.18 9.278z"
+                      ></path>
+                    </svg>
+                  </div>
+                </div>
+                <div className={styles.srchDiv}>
+                  <TextField
+                    onChange={(e) => doChange(e.target.value)}
+                    placeholder="Search or paste any token"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <Search />
+                        </InputAdornment>
+                      ),
+                    }}
+                    variant="standard"
+                    className={styles.srchInput}
+                  />
+                </div>
+                <div className={styles.listDiv}>
+                  {filtered.map((token) => (
+                    <div
+                      key={token.symbol}
+                      className={styles.listItem}
+                      onClick={() => optChanged(token)}
+                    >
+                      <img
+                        src={token.logo}
+                        className={styles.listIcon}
+                        alt="coin-icon"
+                      />
+                      {token.name} -{" "}
+                      <span className={styles.tokenSymbol}>{token.symbol}</span>
+                    </div>
+                  ))}
+                </div>
+              </Collapse>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
     </>
-  )
+  );
 }
